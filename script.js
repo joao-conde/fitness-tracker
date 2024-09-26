1ae39dd6fe5f6bd2a69d61edee0675abb2437a89
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-09-26 19:54:48 +0100

    refactor: update script

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const WEIGHTS_DATA = "data/weights.csv";
 const BODYWEIGHT_DATA = "data/bodyweight.csv";
-
-const CHART_TYPE = "line";
-const DATE_FORMAT = "DD/MM/YYYY";
-
-document.addEventListener("DOMContentLoaded", async () => await mount());
+const DATE_FORMAT_IN = "YYYY-MM-DD";
+const DATE_FORMAT_OUT = "DD/MM/YYYY";
 
 async function mount() {
   const weightsCtx = document.getElementById("weights-chart").getContext("2d");
@@ -12,59 +9,63 @@ async function mount() {
     .getElementById("bodyweight-chart")
     .getContext("2d");
 
-  const exercisesData = await loadCsv(WEIGHTS_DATA);
-  const bodyweightData = await loadCsv(BODYWEIGHT_DATA);
+  const weightsCsv = await loadCsv(WEIGHTS_DATA);
+  const bodyweightCsv = await loadCsv(BODYWEIGHT_DATA);
 
-  createChart(weightsCtx, exercisesData);
-  createChart(bodyweightCtx, bodyweightData);
+  createChart(weightsCtx, weightsCsv);
+  createChart(bodyweightCtx, bodyweightCsv);
+}
+
+function createChart(ctx, csv) {
+  const records = parseCsv(csv);
+  const datasets = groupData(records);
+
+  const options = {
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "day",
+          tooltipFormat: DATE_FORMAT_OUT,
+          displayFormats: {
+            day: DATE_FORMAT_OUT,
+          },
+        },
+        title: {
+          display: true,
+          text: "Date",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Weight (kg)",
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+        position: "top",
+      },
+    },
+  };
+
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: datasets,
+    },
+    options: options,
+  });
+
+  return chart;
 }
 
 async function loadCsv(name) {
   const response = await fetch(name);
   const data = await response.text();
   return data;
-}
-
-function createChart(ctx, csv) {
-  const records = parseCsv(csv);
-  const datasets = createDataset(records);
-  const chart = new Chart(ctx, {
-    type: CHART_TYPE,
-    data: {
-      datasets: datasets,
-    },
-    options: {
-      scales: {
-        x: {
-          type: "time",
-          time: {
-            unit: "day",
-            tooltipFormat: DATE_FORMAT,
-            displayFormats: {
-              day: DATE_FORMAT,
-            },
-          },
-          title: {
-            display: true,
-            text: "Date",
-          },
-        },
-        y: {
-          title: {
-            display: true,
-            text: "Weight (kg)",
-          },
-        },
-      },
-      plugins: {
-        legend: {
-          display: true,
-          position: "top",
-        },
-      },
-    },
-  });
-  return chart;
 }
 
 function parseCsv(data) {
@@ -74,38 +75,34 @@ function parseCsv(data) {
     return {
       label: columns[0],
       y: parseFloat(columns[1]),
-      x: moment(columns[2], "YYYY-MM-DD").format(DATE_FORMAT),
+      x: moment(columns[2], DATE_FORMAT_IN).format(DATE_FORMAT_OUT),
     };
   });
   return records;
 }
 
-function createDataset(data) {
-  const groupedData = data.reduce((acc, entry) => {
-    if (!acc[entry.label]) {
-      acc[entry.label] = [];
+function groupData(data) {
+  const groups = {};
+
+  data.forEach(({ label, y, x }) => {
+    if (!groups[label]) {
+      const red = Math.random() * 255;
+      const green = Math.random() * 255;
+      const blue = Math.random() * 255;
+      groups[label] = {
+        label,
+        color: `rgb(${red}, ${green}, ${blue})`,
+        data: [],
+      };
     }
-    acc[entry.label].push(entry);
-    return acc;
-  }, {});
-
-  const datasets = Object.keys(groupedData).map((label) => {
-    const data = groupedData[label];
-
-    const red = Math.random() * 255;
-    const green = Math.random() * 255;
-    const blue = Math.random() * 255;
-
-    return {
-      label: label,
-      data: data.map((entry) => ({ x: entry.x, y: entry.y })),
-      color: `rgb(${red}, ${green}, ${blue})`,
-    };
+    groups[label].data.push({ x, y });
   });
 
-  return datasets;
+  return Object.values(groups);
 }
 
 function randomBetween(min, max) {
   return Math.random() * (max - min) + min;
 }
+
+document.addEventListener("DOMContentLoaded", async () => await mount());
