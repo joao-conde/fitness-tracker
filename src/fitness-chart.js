9e8055ba08b1b51221bf3c49deab11088fc12082
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-10-18 15:31:07 +0100

    refactor: datasets module for functions that change dataset

--- src/fitness-chart.js
+++ src/fitness-chart.js
@@ -51,46 +51,4 @@ class FitnessChart extends Chart {
     );
     this.update();
   }
-
-  workoutHeaviestSets(data) {
-    // builds map[date][exercise] => heaviest set
-    const heaviestSetMap = data.reduce((acc, row) => {
-      const date = row.date;
-      if (!acc[date]) acc[date] = {};
-
-      const exercise = row.exercise;
-      if (!acc[date][exercise]) acc[date][exercise] = { weight: -Infinity };
-
-      const weight = row.weight;
-      if (weight >= acc[date][exercise].weight) {
-        acc[date][exercise] = row;
-      }
-
-      return acc;
-    }, {});
-
-    // turns the map into a list of heaviest sets
-    const heaviestSets = Object.keys(heaviestSetMap).flatMap((date) =>
-      Object.values(heaviestSetMap[date])
-    );
-
-    return heaviestSets;
-  }
-
-  groupByLabel(data, label, x, y) {
-    // builds map[label] => { label, data }
-    const groupedByLabelMap = data.reduce((acc, row) => {
-      if (!acc[row[label]]) {
-        acc[row[label]] = {
-          label: row[label],
-          data: [],
-        };
-      }
-      acc[row[label]].data.push({ x: row[x], y: row[y] });
-      return acc;
-    }, {});
-
-    // returns a list of [{ label, data }]
-    return Object.values(groupedByLabelMap);
-  }
 }
