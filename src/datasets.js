286e67ac3848319249ec389c2fb8b704894eac38
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-10-20 21:14:39 +0100

    refactor: ordering

--- src/datasets.js
+++ src/datasets.js
@@ -1,4 +1,27 @@
 /**
+ * Takes a list of rows and the name of the label to use and
+ * group data. Each group holds data points with x and y
+ * pairs.
+ */
+function groupByLabel(data, label, x, y) {
+  // builds map[row[label]] => { row[label], [{x, y}] }
+  const groupedByLabelMap = data.reduce((acc, row) => {
+    if (!acc[row[label]]) {
+      acc[row[label]] = {
+        label: row[label],
+        data: [],
+      };
+    }
+    acc[row[label]].data.push({ x: row[x], y: row[y] });
+    return acc;
+  }, {});
+
+  // returns a list of [{ row[label], [{x, y}] }]
+  const groups = Object.values(groupedByLabelMap);
+  return groups;
+}
+
+/**
  * Takes a list of rows and finds out the row corresponding to the
  * heaviest set of one exercise for one workout.
  *
@@ -26,27 +49,4 @@ function workoutHeaviestSets(data) {
     Object.values(heaviestSetMap[date])
   );
   return heaviestSets;
-}
-
-/**
- * Takes a list of rows and the name of the label to use and
- * group data. Each group holds data points with x and y
- * pairs.
- */
-function groupByLabel(data, label, x, y) {
-  // builds map[label] => { label, data }
-  const groupedByLabelMap = data.reduce((acc, row) => {
-    if (!acc[row[label]]) {
-      acc[row[label]] = {
-        label: row[label],
-        data: [],
-      };
-    }
-    acc[row[label]].data.push({ x: row[x], y: row[y] });
-    return acc;
-  }, {});
-
-  // returns a list of [{ label, data }]
-  const groups = Object.values(groupedByLabelMap);
-  return groups;
 }
