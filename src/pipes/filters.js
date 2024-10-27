b3a488221df93a6dd25cfca7d1e71345e5726e8d
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-10-27 18:12:53 +0000

    refactor: pipes filters and transforms

---
+++ src/pipes/filters.js
@@ -0,0 +1,38 @@
+const EXERCISE_INCLUDES = [
+  "bench",
+  "squat",
+  "deadlift",
+  "pull up",
+  "bicep",
+  "tricep",
+];
+
+const EXERCISE_EXCLUDES = [
+  "decline",
+  "bodyweight",
+  "close grip",
+  "assisted",
+  "cable",
+  "rope tricep pushdown",
+];
+
+const MIN_FREQUENCY = 50;
+
+function filterLabelByFrequency(rows, label, minFrequency) {
+  const groupedByLabel = groupByLabel(rows, label, null, null);
+
+  const counts = groupedByLabel.reduce((acc, group) => {
+    acc[group.label] = group.data.length;
+    return acc;
+  }, {});
+
+  return rows.filter((row) => counts[row[label]] >= minFrequency);
+}
+
+function filterLabelByValue(rows, label, includes, excludes) {
+  return rows.filter(
+    (row) =>
+      includes.some((i) => row[label].includes(i)) &&
+      excludes.every((e) => !row[label].includes(e))
+  );
+}
