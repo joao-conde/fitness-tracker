7498a5661fb29ea45b75f20242bb354b58e870e2
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-11-08 20:32:56 +0000

    feat: working bundler

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,16 +1,30 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.17.19/mod.js";
 
+const removeNpmImports: esbuild.Plugin = {
+  name: "removeNpmImports",
+  setup(build: esbuild.PluginBuild) {
+    build.onLoad({ filter: /.ts/ }, async (args) => {
+      const source = await Deno.readTextFile(args.path);
+
+      const transformedSource = source
+        .split("\n")
+        .filter((line) => !line.includes("npm:"))
+        .join("\n");
+
+      return {
+        contents: transformedSource,
+        loader: "ts",
+      };
+    });
+  },
+};
+
 await esbuild.build({
   entryPoints: ["src/main.ts"],
+  outfile: "dist/bundle.js",
   bundle: true,
-  external: ["npm:@types/chartjs"],
-  outfile: "bundle.js",
-  platform: "browser",
-  format: "esm",
-  target: ["esnext"],
-  minify: false,
-  sourcemap: false,
-  define: { "process.env.NODE_ENV": '"production"' },
+  minify: true,
+  plugins: [removeNpmImports],
 });
 
 esbuild.stop();
